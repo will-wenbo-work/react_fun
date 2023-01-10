dca23a4bf73d898e717687836f47ac2628ddba13
will-wenbo-work <willwenbowork@gmail.com>
2023-01-09 22:54:20 -0800

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       <p>play with react and test my git config??</p>
+      <p>seems I do not need validation when git push??</p>
     </div>
   );
 }
